5616db495a18e777aaf2d03ed69df8e9dd9c8aff
hanifullah1143 <hanifullah1143@gmail.com>
2026-09-22 18:51:39 +0500

    p

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+      return (
+          <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "sans-serif" }}>
+                <h1>Welcome to IqamaGuard</h1>
+                      <p>App is running successfully!</p>
+                          </div>
+                            );
+                            }
+                            
+}
